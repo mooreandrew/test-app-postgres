392d2fbde52a029783082a7f92801a631a821d80
Andrew Moore <mooreandrewlr@gmail.com>
2017-05-12 22:30:54 +0100

    fixed missing uri

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 import os
 from flask.ext.sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 app.config.from_object(os.environ.get('SETTINGS'))
+
+db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config(object):
     DEBUG = False
     LOGGING_PATH = os.getenv('LOGGING_PATH', 'python_logging/logging.yaml')
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
 
 class DevelopmentConfig(Config):
     TESTVALUE = os.environ['TESTVALUE']
