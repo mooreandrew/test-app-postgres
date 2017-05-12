834d1ab26763627c12a8bec891d23abbaf9f6cc6
Andrew Moore <mooreandrewlr@gmail.com>
2017-05-12 22:55:49 +0100

    removed pg8000

--- environment.sh
+++ environment.sh
@@ -1,3 +1,3 @@
 export TESTVALUE='test'
 export SETTINGS='config.DevelopmentConfig'
-export SQLALCHEMY_DATABASE_URI='postgresql+pg8000://tester:tester@192.168.42.91/testdata'
+export SQLALCHEMY_DATABASE_URI='postgresql://tester:tester@192.168.42.91/testdata'
