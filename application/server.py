80c08d85be0ca177ae0f20f74c31ff1443e0c5b7
Andrew Moore <mooreandrewlr@gmail.com>
2017-05-12 22:19:50 +0100

    added sql command

--- application/server.py
+++ application/server.py
@@ -1,10 +1,26 @@
-from application import app
+from application import app, db
 import os
-from flask import Flask
+from flask import Flask, render_template, request
+import traceback
+import sys
+from sqlalchemy.sql import text
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return(app.config['TESTVALUE'])
+
+    status = 'N/A'
+    sql = ''
+    if 'sql' in request.form:
+        sql = request.form['sql']
+        try:
+            sql = request.form['sql']
+            db.session.execute(text(sql))
+            status = 'Success'
+        except:
+
+            status = traceback.format_exc()
+
+    return render_template('index.html', status=status, sql=sql)
 
 
 @app.route('/health')
--- environment.sh
+++ environment.sh
@@ -1,2 +1,3 @@
 export TESTVALUE='test'
 export SETTINGS='config.DevelopmentConfig'
+export SQLALCHEMY_DATABASE_URI='postgresql+pg8000://tester:tester@192.168.42.91/testdata'
